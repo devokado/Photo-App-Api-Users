838b7ad2717a6a612a48763c0d39b42f92f99422
KayTee9 <tajfar.khatere@yahoo.com>
2020-11-09 14:12:48 +0330

    8. Zuul as a Load Balancer

--- src/main/java/com/devacado/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/com/devacado/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class PhotoAppApiUsersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+    }
 
 }
--- src/main/java/com/devacado/photoapp/api/users/ui/controller/UsersController.java
+++ src/main/java/com/devacado/photoapp/api/users/ui/controller/UsersController.java
@@ -1,5 +1,7 @@
-package ui.controllers;
+package com.devacado.photoapp.api.users.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String checkStatus() {
-        return "Working";
+        return "Working on port" + environment.getProperty("local.server.port");
     }
 }
