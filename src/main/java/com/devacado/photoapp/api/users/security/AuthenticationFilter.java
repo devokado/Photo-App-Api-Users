561780fc32de893d523c53cd90eb5fe4c2864904
KayTee9 <tajfar.khatere@yahoo.com>
2020-11-24 18:11:59 +0100

    11. Users Microservice - Implementing User Login

---
+++ src/main/java/com/devacado/photoapp/api/users/security/AuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.devacado.photoapp.api.users.security;
+
+import com.devacado.photoapp.api.users.service.UsersService;
+import com.devacado.photoapp.api.users.shared.UserDTO;
+import com.devacado.photoapp.api.users.ui.model.LoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private UsersService usersService;
+    private Environment environment;
+
+    public AuthenticationFilter(UsersService usersService, Environment environment, AuthenticationManager authenticationManager) {
+        this.usersService = usersService;
+        this.environment = environment;
+        super.setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+
+            LoginRequestModel creds = new ObjectMapper()
+                    .readValue(request.getInputStream(), LoginRequestModel.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String username = ((User) authResult.getPrincipal()).getUsername();
+        UserDTO userDetails = usersService.getUserDetailsByEmail(username);
+        String token = Jwts.builder()
+                .setSubject(userDetails.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
+                .signWith(SignatureAlgorithm.HS512, environment.getProperty("token.secret") )
+                .compact();
+
+        response.addHeader("token", token);
+        response.addHeader("userId", userDetails.getUserId());
+    }
+}
--- src/main/java/com/devacado/photoapp/api/users/service/UsersService.java
+++ src/main/java/com/devacado/photoapp/api/users/service/UsersService.java
@@ -1,7 +1,9 @@
 package com.devacado.photoapp.api.users.service;
 
 import com.devacado.photoapp.api.users.shared.UserDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
     UserDTO createUser(UserDTO userDetails);
+    UserDTO getUserDetailsByEmail(String email);
 }
